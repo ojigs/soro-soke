fe101b414210f0e1a4af765ab39706a674878b68
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-27 17:53:22 +0100

    implement send message component and emit socket event on send message

--- client/src/pages/chat/index.js
+++ client/src/pages/chat/index.js
@@ -1,11 +1,13 @@
 import styles from "./styles.module.css";
 import Message from "./messages";
+import SendMessage from "./send-message";
 
-const Chat = ({ socket }) => {
+const Chat = ({ socket, username, room }) => {
   return (
     <div className={styles.chatContainer}>
       <div>
         <Message socket={socket} />
+        <SendMessage socket={socket} username={username} room={room} />
       </div>
     </div>
   );
---
+++ client/src/pages/chat/send-message.js
@@ -0,0 +1,31 @@
+import styles from "./styles.module.css";
+import React, { useState } from "react";
+
+const SendMessage = ({ username, room, socket }) => {
+  const [message, setMessage] = useState("");
+
+  function sendMessage() {
+    if (message !== "") {
+      const __createdTime__ = Date.now();
+      socket.emit("send_message", { username, room, message, __createdTime__ });
+      setMessage("");
+    }
+  }
+
+  return (
+    <div className={styles.sendMessageContainer}>
+      <input
+        type="text"
+        className="{styles.messageInput"
+        placeholder="Message..."
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+      />
+      <button className="btn btn-primary" onClick={sendMessage}>
+        Send Message
+      </button>
+    </div>
+  );
+};
+
+export default SendMessage;
